b6096ef348be8a8c34ecb0ab470e5e1d4f6942aa
Hans Yang <545872067@qq.com>
2018-11-12 00:35:32 +0800

    添加了task.run

---
+++ task run1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_run1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task.run 无参数，无返回值使用  //因为 task.run 的参数是 Action，并没有Action<int ,int> 所以不能有参数
+            //Task.Run(
+            //    () =>{ Console.WriteLine("hello world"); }
+            //    );
+            //Console.ReadLine();
+
+           Task<int> t1=   Task<int>.Run<int>(
+                () =>{ return 1 + 2; }
+                );
+           var result= t1.Result;
+           Console.WriteLine(result);
+           Console.ReadLine();
+
+        }
+     
+    }
+}
---
+++ task6 taskfactory实例开启/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task6_taskfactory实例开启
+{
+    class Program
+    {
+        //6.taskfactory 实例开启
+        static void Main(string[] args)
+        {
+            TaskFactory tf1 = new TaskFactory();
+            Task t1 = tf1.StartNew(SayHi);
+            Console.ReadLine();
+        }
+        static void SayHi()
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
